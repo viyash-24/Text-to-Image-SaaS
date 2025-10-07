d2aefad82e848cb9ba9359818426b0938c6ac87b
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-07 08:04:39 +0530

    pages routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Home from './pages/Home';
+import Result from './pages/Result';
+import BuyCredit from './pages/BuyCredit';
 
 const App = () => {
   return (
     <div>
-      
+     <Home/>
+     <Result/>
+     <BuyCredit/> 
     </div>
   );
 }
