107e66071186ae48f52005d3a9d500cfe9a1e8dd
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-09 21:49:56 +0530

    routes files created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home';
 import Result from './pages/Result';
 import BuyCredit from './pages/BuyCredit';
 
 const App = () => {
   return (
-    <div>
-     <Home/>
-     <Result/>
-     <BuyCredit/> 
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 
+    min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/result' element={<Result/>}/>
+        <Route path='/buycredit' element={<BuyCredit/> }/>       
+      </Routes>
+    
     </div>
   );
 }
