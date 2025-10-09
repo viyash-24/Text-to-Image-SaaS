4a81173375f7564e27a37bf7659745e7666af0ba
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-09 23:20:39 +0530

    onclick function added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
           {
           user ?
           <div className='flex items-center gap-2 sm:gap-3'>
-                    <button className='flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700'>
+                    <button  onClick={()=>navigate('/buy')} className='flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700'>
                               <img className='w-5 ' src={assets.credit_star} alt="" />
                               <p className='text-xs sm:text-sm font-medium text-gray-600'> Credits left : 50</p>
                     </button>
