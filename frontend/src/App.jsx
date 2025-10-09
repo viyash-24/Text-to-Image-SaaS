bb4c0bff029c132efea11552565a4bfdb9407e00
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-09 22:58:30 +0530

    Navbar file import in a App.jsx and AppContextProvider is imported in a main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home';
 import Result from './pages/Result';
 import BuyCredit from './pages/BuyCredit';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 
-    min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+    min-h-screen bg-gradient-to-b from-teal-50 to-orange-100'>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/result' element={<Result/>}/>
