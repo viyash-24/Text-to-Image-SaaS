347454969b4c2a6d0862e88271c3a5a463289dde
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-10 11:46:01 +0530

    home page under structer implemented in Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -10,6 +10,21 @@ const Header = () => {
           </p>
           <img src={assets.star_icon} alt="" />
       </div>
+      <h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'>Turn text to <span className='text-blue-600'>image</span>, in seconds.</h1>
+      <p className='text-center max-w-xl mx-auto mt-5'> unleash your creativity with AI. Turn your imagination into visual art in seconds -just type, and watch the magic happen.</p>
+    <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'>
+      Genarate Images
+      <img className="h-6" src={assets.star_group} alt="" />
+    </button>
+
+    <div className='flex flex-wrap justify-center mt-16 gap-3'>
+      {Array(6).fill('').map((item,index)=>(
+        <img className=' rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10 '
+         src={index % 2 ===0 ? assets.sample_img_2 :assets.sample_img_1 } 
+         alt=""  key={index} width={70}/>
+      ))}
+    </div>
+    <p className='mt-2 text-neutral-600'>Generated images from imagify</p>
     </div>
   );
 }
