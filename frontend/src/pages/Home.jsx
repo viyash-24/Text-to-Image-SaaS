27bbcf88840af8ae4c5029d89d7f1d55a4a2b142
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-13 08:34:28 +0530

    Steps and Description components are imported

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header'
 import Steps from '../components/Steps';
+import Description from '../components/Description';
 const Home = () => {
   return (
     <div>
     < Header/>
     <Steps/>
+    <Description/>
     </div>
   );
 }
