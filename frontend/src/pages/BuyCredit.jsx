9954d27656e59e832cbba4beedb3e26fd9919a30
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-17 18:45:33 +0530

    buyCredit page is started creating

--- frontend/src/pages/BuyCredit.jsx
+++ frontend/src/pages/BuyCredit.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { plans} from '../assets/assets';
 
 const BuyCredit = () => {
   return (
-    <div>
-      Buy
+    <div className='min-h-[80vh] text-center pt-14 mb-10'>
+      <button className='border border-gray-400 px-10 py-2 rounded-full mb-6'>Our Plans  </button>
+      <h1 className='text-center text-3xl font-medium mb-6 sm:mb-10'> Choose the plan</h1>
+      <div className='flex'>
+        {plans.map(()=>{})}
+
+      </div>
     </div>
   );
 }
