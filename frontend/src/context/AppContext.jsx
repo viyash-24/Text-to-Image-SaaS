a38dd6fd4bd16beb0581b153765967cd418da235
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-18 17:11:53 +0530

    login fu

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -4,9 +4,10 @@ export const  AppContext = createContext()
 
 const AppContextProvider=(props)=>{
           const [user, setUser]= useState(null)
+          const [showLogin,setShowLogin]=useState('false')
 
           const value={
-                    user,setUser
+                    user,setUser,showLogin,setShowLogin
           }
 
           return (
