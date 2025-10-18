0e56175db35c0194037c01c13b12a8827d7c7b81
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-18 17:15:06 +0530

    login component is added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home';
 import Result from './pages/Result';
 import BuyCredit from './pages/BuyCredit';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Login from './components/Login';
+import { AppContext } from './context/AppContext';
 
 const App = () => {
+
+  const {showLogin} = useContext(AppContext)
+  
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 
     min-h-screen bg-gradient-to-b from-teal-50 to-orange-100'>
       <Navbar/>
+      { showLogin && <Login/>}
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/result' element={<Result/>}/>
