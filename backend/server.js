7eec080339d63de5466ad5469926ef932fb1326b
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-21 16:41:23 +0530

    mongoDB file is imported in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
 
 const PORT =process.env.PORT || 4000
 const app =express()
 
 app.use(express.json())
 app.use(cors())
+
+await connectDB()
 
 app.get('/',(req,res)=>res.send ("API working"))
 
