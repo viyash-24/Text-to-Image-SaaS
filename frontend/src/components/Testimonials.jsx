f1d9b8cc945b2bf45aa6619b010467bf13e541d8
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-21 13:35:19 +0530

    motion animation added

--- frontend/src/components/Testimonials.jsx
+++ frontend/src/components/Testimonials.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { assets, testimonialsData } from '../assets/assets';
+import {motion } from 'framer-motion'
 
 const Testimonials = () => {
   return (
-     <div className='flex flex-col items-center justify-center my-20 py-12 '>
+     <motion.div 
+          initial={{opacity:0.2, y:100}}
+         transition={{duration:1}}
+         whileInView={{opacity :1,y:0}}
+         viewport={{once:true}}
+         
+        className='flex flex-col items-center justify-center my-20 py-12 '>
           <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Customer testimonials</h1>
       <p className='text-gray-500 mb-12'>What Our Users Are Saying</p>
 
@@ -27,7 +34,7 @@ const Testimonials = () => {
 
       </div>
 
-    </div>
+    </motion.div>
   );
 }
 
