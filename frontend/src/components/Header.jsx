619873687a94c94ee12917265321e01565aa6220
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-21 13:22:56 +0530

    motion animation added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useContext} from 'react';
 import { assets } from '../assets/assets';
 import {motion} from "framer-motion"
 import { AppContext } from '../context/AppContext';
