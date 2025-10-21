c9d2cbd44f533e451a38e53ad0bd0ad7fa06854a
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-21 14:04:21 +0530

    motion animation added

--- frontend/src/pages/Result.jsx
+++ frontend/src/pages/Result.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { assets } from '../assets/assets';
+import { motion } from 'framer-motion'
 
 const Result = () => {
 
@@ -7,13 +8,20 @@ const Result = () => {
   const [isImageLoaded,setIsImageLoaded]=useState(false)
   const [loading,setLoading]=useState(false)
   const [input,setInput]=useState('')
+
   const onSubmitHandler=async (e)=>{
     
 
   }
 
   return (
-    <form onSubmit={onSubmitHandler} className='flex flex-col min-h-[90vh] justify-center items-center'>
+    <motion.form
+    initial={{opacity:0.2, y:100}}
+    transition={{duration:1}}
+    whileInView={{opacity :1,y:0}}
+    viewport={{once:true}}
+    
+    onSubmit={onSubmitHandler} className='flex flex-col min-h-[90vh] justify-center items-center'>
     <div>
       <div className='relative'>
         <img src={image} className="max-w-sm rounded"alt="" />
@@ -43,7 +51,7 @@ const Result = () => {
       <a href={image} download className='bg-zinc-900 px-10 py-3 rounded-full cursor-pointer' >Download</a>
     </div>
 }
-    </form>
+    </motion.form>
   );
 }
 
