95046a34752d95d1370d7b14f0c2b4c6e055aaea
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-21 13:33:49 +0530

    motion animation added

--- frontend/src/components/Description.jsx
+++ frontend/src/components/Description.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { assets } from '../assets/assets';
+import {motion } from 'framer-motion'
 
 const Description = () => {
   return (
-    <div className='flex flex-col items-center justify-center my-24 p-6 md:px-28 '>
+    <motion.div 
+     initial={{opacity:0.2, y:100}}
+    transition={{duration:1}}
+    whileInView={{opacity :1,y:0}}
+    viewport={{once:true}}
+    
+    className='flex flex-col items-center justify-center my-24 p-6 md:px-28 '>
       <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Create AI Images</h1>
       <p className='text-gray-500 mb-8'>Turn your imagination into visuals</p>
       <div className='flex flex-col gap-5 md:gap-14 md:flex-row items-center'>
@@ -17,7 +24,7 @@ const Description = () => {
                        effortlessly. Powered by advanced AI technology, thec reative possibilities are limitless!k </p>
           </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
