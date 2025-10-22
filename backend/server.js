5b3653a8bafb01672c5d10be51e7df92f776a256
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-22 12:18:29 +0530

    userRoutes is  added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 const PORT =process.env.PORT || 4000
 const app =express()
@@ -11,6 +12,8 @@ app.use(cors())
 
 await connectDB()
 
+
+app.use('/api/user',userRouter)
 app.get('/',(req,res)=>res.send ("API working"))
 
 app.listen(PORT ,()=>console.log(`server running on port ${PORT}`))
