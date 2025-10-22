a90cdc799e4922c4c1de2e0dd685a5c485f180c7
viyash-24 <viyashviyashan2417@gmail.com>
2025-10-22 13:34:55 +0530

    register spelling  is corrected

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,7 +4,7 @@ import {registerUser,loginUser} from '../controllers/userController.js'
 
 const userRouter =express.Router()
 
-userRouter.post('/resgister',registerUser)
+userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
 
 export default userRouter
