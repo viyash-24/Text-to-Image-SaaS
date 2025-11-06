52ed62db9a6c3363df58e37e2a4cb2fc0231d252
viyash-24 <viyashviyashan2417@gmail.com>
2025-11-06 12:29:42 +0530

    login functions completed

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,13 +1,49 @@
 import React, { useContext, useEffect, useState } from 'react';
 import {assets}  from '../assets/assets'
 import { AppContext } from '../context/AppContext';
-
+import { motion } from 'framer-motion'
+import axios from 'axios'
+import { toast } from 'react-toastify';
 const Login = () => {
 
    const [state,setState]=useState('Login')
-   const {setShowLogin} =useContext(AppContext)
+   const {setShowLogin,backendUrl,setToken,setUser} =useContext(AppContext)
+  const[name,setName]=useState('')
+  const[email,setEmail]=useState('')
+  const[password,setPassword]=useState('')
 
 
+  const onSubmitHandler=async(e)=>{
+    e.preventDefault()
+
+    try {
+      if(state==='Login'){
+        const {data} = await axios.post(backendUrl +'/api/user/login',{email,password})
+     if(data.success){
+       setToken(data.token)
+       setUser(data.user)
+       localStorage.setItem('token',data.token)
+       setShowLogin(false)
+     }else{
+      toast.error(data.message)
+     }
+     
+    }else{
+      const {data} = await axios.post(backendUrl +'/api/user/register',{name,email,password})
+     if(data.success){
+       setToken(data.token)
+       setUser(data.user)
+       localStorage.setItem('token',data.token)
+       setShowLogin(false)
+     }else{
+      toast.error(data.message)
+     }
+    }
+      
+    } catch (error) {
+       toast.error(error.message)
+    }
+  }
    useEffect(()=>{
           document.body.style.overflow='hidden';
 
@@ -19,24 +55,31 @@ const Login = () => {
   return (
     <div className='fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
 
-    <form className='relative bg-white p-10 rounded-xl text-slate-500'>
+    <motion.form 
+    onSubmit={onSubmitHandler}
+    initial={{opacity:0.2, y:50}}
+    transition={{duration:0.3}}
+    whileInView={{opacity :1,y:0}}
+    viewport={{once:true}}
+
+    className='relative bg-white p-10 rounded-xl text-slate-500'>
           <h1 className='text-center text-2xl text-neutral-700 font-medium '>{state}</h1>
           <p className='text-sm'>Welcome back! Please sign in to continue </p>
 
          {state !== 'Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
              <img className='w-5' src={assets.profile_icon} alt="" />  
-             <input className='outline-none text-sm' type="text" placeholder='Full Name' required />    
+             <input onChange={e=>setName(e.target.value)} value={name} className='outline-none text-sm' type="text" placeholder='Full Name' required />    
           </div>
           }
 
            <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
              <img src={assets.email_icon} alt="" />  
-             <input className='outline-none text-sm' type="email" placeholder='Email ID' required />    
+             <input onChange={e=>setEmail(e.target.value)} value={email} className='outline-none text-sm' type="email" placeholder='Email ID' required />    
           </div>
 
            <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
              <img src={assets.lock_icon} alt="" />  
-             <input className='outline-none text-sm' type="password" placeholder='Password' required />    
+             <input onChange={e=>setPassword(e.target.value)} value={password}  className='outline-none text-sm' type="password" placeholder='Password' required />    
           </div>
 
           <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot Password ?</p>
@@ -48,7 +91,7 @@ const Login = () => {
           <p className='mt-5 text-center'>Already have an account? <span className='text-blue-600 cursor-pointer' onClick={()=>setState('Login')}>Login</span></p>}
 
           <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} className='absolute top-5  right-5 cursor-pointer' alt="" />
-    </form>
+    </motion.form>
 
       
     </div>
