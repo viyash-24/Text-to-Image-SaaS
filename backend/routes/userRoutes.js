9a33733bbe8bf716560e9f0187789fb677df4cd1
viyash-24 <viyashviyashan2417@gmail.com>
2025-11-06 12:40:22 +0530

    credits and razor pay routes are is imported

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express'
-import {registerUser,loginUser} from '../controllers/userController.js'
+import {registerUser,loginUser, userCredits, paymentRazorpay,verifyRazorpay} from '../controllers/userController.js'
+import  userAuth from '../middlewares/auth.js'
 
 
 const userRouter =express.Router()
 
 userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
+userRouter.get('/credits',userAuth,userCredits)
+userRouter.post('/pay-razor',userAuth,paymentRazorpay)
+userRouter.post('/verify-razor',verifyRazorpay)
 
-export default userRouter
+
+
+export default userRouter 
