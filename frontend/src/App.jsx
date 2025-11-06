d9c49ba83e0e169653bcb38aa7579cd626dd94a2
viyash-24 <viyashviyashan2417@gmail.com>
2025-11-06 12:34:59 +0530

    toastify imported

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react';
 import {Routes,Route} from 'react-router-dom'
+import {ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 import Home from './pages/Home';
 import Result from './pages/Result';
 import BuyCredit from './pages/BuyCredit';
@@ -15,6 +18,7 @@ const App = () => {
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 
     min-h-screen bg-gradient-to-b from-teal-50 to-orange-100'>
+      <ToastContainer position='bottom-right'/>
       <Navbar/>
       { showLogin && <Login/>}
       <Routes>
