507a9c82b15ea0a3eff28b3c9cbac08c367e8b47
viyash-24 <viyashviyashan2417@gmail.com>
2025-11-11 14:23:04 +0530

    clipdrop url fix

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -19,7 +19,7 @@ export const generateImage = async(req,res)=>{
              const formData =new FormData() 
              formData.append('prompt',prompt)
 
-            const {data}= await axios.post('https://clipdrop-api.com/text-to-image/v1',formData,{
+            const {data}= await axios.post('https://clipdrop-api.co/text-to-image/v1',formData,{
                   headers: {
                         'x-api-key': process.env.CLIPDROP_API,
                   },
